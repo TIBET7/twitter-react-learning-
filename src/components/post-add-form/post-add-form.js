e2a523c1319cc082edcd697a05f53badca7d2d51
Dim <om1234777@gmail.com>
2020-08-29 13:03:07 +0300

    add complete app

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,22 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import '../post-add-form/post-add-form.css';
 
-const PostAddForm = ({onAdd}) => {
-    return (
-        <div action="" className="bottom-panel d-flex">
-            <input 
-                className="form-control new-post-label"
-                type="text"
-                placeholder="О чем Вы думаете сейчас?"
-            />
-            <button 
-                className="btn btn-outline-secondary"
-                tepe="submit"
-                onClick={() => onAdd('Hello')}>
-                Добавить</button>
-        </div>
-    )
-};
+export default class PostAddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+        this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
 
-export default PostAddForm;
+    onValueChange(event) {
+        this.setState({
+            text: event.target.value
+        });
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        this.props.onAdd(this.state.text)
+        this.setState({
+            text: ''
+        });
+    }
+    
+    render () {
+        return (
+            <form 
+                className="bottom-panel d-flex"
+                onSubmit={this.onSubmit}>
+                <input 
+                    className="form-control new-post-label"
+                    type="text"
+                    placeholder="О чем Вы думаете сейчас?"
+                    onChange={this.onValueChange}
+                    value={this.state.text}
+                />
+                <button 
+                    className="btn btn-outline-secondary"
+                    tepe="submit">
+                    Добавить</button>
+            </form>
+        )
+    }
+}
+    
--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -3,52 +3,31 @@ import React, {Component} from 'react';
 import '../post-list-item/post-list-item.sass';
 
 export default class PostListItem extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            important: false,
-            like: false
-        }
-        this.onImportant = this.onImportant.bind(this);
-        this.onLike = this.onLike.bind(this);
-    }
-
-    //state.important деструктурируем ниже в {important}
-
-    onImportant() {
-        this.setState(({important}) => ({
-            important: !important,   //т.е. если important было true, то станет false, и наоборот
-        }));
-    }
-    
-    onLike() {
-        this.setState(({like}) => ({
-            like: !like,   //т.е. если important было true, то станет false, и наоборот
-        }));
-    }
 
     render() {
-        const {label, onDelete} = this.props;
-        const {important, like} = this.state;
+        const {label, onDelete, onToggleImportant, onToggleLiked, important, like} = this.props;
         let classNames = 'app-list-item d-flex justify-content-between';
+
         if (important) {
             classNames += ' important';
         }
+
         if (like) {
             classNames += ' like';
         }
+
         return (
         <div className={classNames}>
             <span 
             className="app-list-item-label" 
-            onClick={this.onLike}>
+            onClick={onToggleLiked}>
                 {label}
             </span> 
             <div className="d-flex justify-content-center align-items-center">
                 <button 
                     className="btn-star btn-sm" 
                     type="button"
-                    onClick={this.onImportant}>
+                    onClick={onToggleImportant}>
                     <i className="fa fa-star"></i>
                 </button>
                 <button 
@@ -60,6 +39,6 @@ export default class PostListItem extends Component {
                 <i className="fa fa-heart"></i>
             </div>          
         </div>
-        )
+        );
     }
 }
